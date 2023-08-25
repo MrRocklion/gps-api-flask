79760ec63e1a741f2004ff8a434f931a1a92d758
David Diaz <david.diaz190799@gmail.com>
2023-08-24 22:31:10 -0500

    se agrego hello world

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ ref = db.reference('dispositivos')
 app = Flask(__name__)
 
 
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
 @app.route('/ping')
 def ping():
     return jsonify({"message":"agregado"})
