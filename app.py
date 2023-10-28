5bbb88ee8d2afbf486e664312d84c9f6b8102fac
David Diaz <david.diaz190799@gmail.com>
2023-10-28 17:44:00 -0500

    se agrega condicional para que funcione mejor

--- app.py
+++ app.py
@@ -31,25 +31,26 @@ def setPosition():
     args.to_dict()
     id = args.get("id")
     data = args.get("data")
+    latitud = 0
+    longitud = 0
     string_empty = data.replace(" ", "")
     string_formated = string_empty[9:].split(",")
-    print(string_formated)
-    grados_lat = string_formated[0][0:2]
-    grados_lon = string_formated[2][0:3]
-    minutos_lat = float(string_formated[0][2:])
-    minutos_lon = float(string_formated[2][3:])
-    latitud = int(grados_lat) + ((minutos_lat) / 60)
-    longitud =  int(grados_lon) + ((minutos_lon) / 60)
-    if string_formated[1] =="S":
-        latitud =  latitud*-1
-    if string_formated[3] =="W":
-        longitud =  longitud*-1
-    device_ref = ref.child(id)
-    device_ref.update({
-    'lat':latitud,
-    'lon':longitud
-    })
-
+    if string_formated[0]!="" and string_formated[2]!="":
+        grados_lat = string_formated[0][0:2]
+        grados_lon = string_formated[2][0:3]
+        minutos_lat = float(string_formated[0][2:])
+        minutos_lon = float(string_formated[2][3:])
+        latitud = int(grados_lat) + ((minutos_lat) / 60)
+        longitud =  int(grados_lon) + ((minutos_lon) / 60)
+        if string_formated[1] =="S":
+            latitud =  latitud*-1
+        if string_formated[3] =="W":
+            longitud =  longitud*-1
+        device_ref = ref.child(id)
+        device_ref.update({
+        'lat':latitud,
+        'lon':longitud
+        })
     return jsonify({"latitud":latitud,"longitud":longitud})
 
 
